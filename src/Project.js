a08db3eed6105fa3ba0e38a9fdd5f0fcb2b70631
Somoye123 <somoye.ayotunde@gmail.com>
2020-09-17 12:05:22 -0700

    Add Project class with required properties

---
+++ src/Project.js
@@ -0,0 +1,26 @@
+export default class Project {
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get tasks() {
+    return this._tasks;
+  }
+
+  set tasks(tasks) {
+    this._tasks = tasks;
+  }
+
+  addtask(task) {
+    this._tasks.push(task);
+  }
+}

738bd490feea34f46f5aaef3d9897f20271f334d
somoye123 <somoye.ayotunde@gmail.com>
2020-09-21 15:03:50 +0100

    add default tasks for defaults projects

--- src/initializeProject.js
+++ src/initializeProject.js
@@ -1,10 +1,32 @@
 import Project from "./Project";
+import Task from "./Task";
 import { setProjects, getProjects } from "./localStorageModule";
 
 export default function setDefaultProject() {
   const defaultproject = new Project("Example Project");
   const defaultproject2 = new Project("Example Project2");
-  let testing = [defaultproject, defaultproject2];
-  setProjects(testing);
-  return testing;
+  const defaultprojectTasks = new Task(
+    "adding a new project",
+    "adding a new project description",
+    "task due date",
+    "high",
+    "not yet completed",
+    "to be done as soon as possible"
+  );
+
+  const defaultproject2Tasks = new Task(
+    "adding another new project",
+    "adding another new project description",
+    "task due date",
+    "low",
+    "completed",
+    "to be done as soon as possible"
+  );
+
+  defaultproject.addTask(defaultprojectTasks);
+  defaultproject2.addTask(defaultproject2Tasks);
+
+  const defaultProjects = [defaultproject, defaultproject2];
+  setProjects(defaultProjects);
+  return defaultProjects;
 }

6b3f4331f01b5eae9c62f9f1548804299972fb41
somoye123 <somoye.ayotunde@gmail.com>
2020-09-22 12:36:26 +0100

    build submitprojectForm event listener

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,8 +1,16 @@
-import { setSelectedProject, getSelectedProject } from "./localStorageModule";
+import {
+  setSelectedProject,
+  getProjects,
+  setProjects,
+} from "./localStorageModule";
+import Project from "./Project";
 
 import BuildPage from "./buildPage";
+import projects from "./projects";
+import buildPage from "./buildPage";
 
-const eventListeners = function () {
+const eventListeners = () => {
+  const formProject = () => document.getElementById("new-project");
   const chooseProject = () => {
     document.getElementById("selected-project").addEventListener(
       "change",
@@ -20,30 +28,33 @@ const eventListeners = function () {
 
   const addProjectButton = () => {
     document.getElementById("add-project").addEventListener("click", () => {
-      document.getElementById("new-project").style.display = "block";
+      formProject().style.display = "block";
     });
   };
 
   const submitProjectForm = () => {
-    //     const formProject = document.getElementById("new-project");
-    // formProject.addEventListener("submit", (event) => {
-    //   event.preventDefault();
-    //   const title = formProject.elements.namedItem("title").value;
-    //   const project = new Project(title);
-    //   if (title) {
-    //     projects.push(project);
-    //     localStorage.projects = JSON.stringify(projects);
-    //     formProject.reset();
-    //     formProject.style.display = "none";
-    //   } else {
-    //     alert("Fill all informations correctly ");
-    //   }
-    // });
+    formProject().addEventListener("submit", (event) => {
+      event.preventDefault();
+      const title = formProject().elements.namedItem("title").value;
+      if (title) {
+        const allProject = getProjects();
+        const project = new Project(title);
+        allProject.push(project);
+        setProjects(allProject);
+        formProject().reset();
+        formProject().style.display = "none";
+        // location.reload();
+        // buildPage();
+      } else {
+        alert("Fill all informations correctly ");
+      }
+    });
   };
 
   return {
     chooseProject,
     addProjectButton,
+    submitProjectForm,
   };
 };
 

5d45e2e0d21be9005955f6e12a86960559611d80
somoye123 <somoye.ayotunde@gmail.com>
2020-09-22 19:20:36 +0100

    refactor storageModule to correctly implement module design pattern

--- src/localStorageModule.js
+++ src/localStorageModule.js
@@ -1,15 +1,19 @@
-const getProjects = () =>
-  localStorage.projects && JSON.parse(localStorage.projects);
+// export { getProjects, getSelectedProject, setProjects, setSelectedProject };
+const storage = () => {
+  const getProjects = () =>
+    localStorage.projects && JSON.parse(localStorage.projects);
 
-const getSelectedProject = () =>
-  localStorage.selectedProject && JSON.parse(localStorage.selectedProject);
+  const getSelectedProject = () =>
+    localStorage.selectedProject && JSON.parse(localStorage.selectedProject);
 
-const setProjects = (projects) => {
-  localStorage.projects = JSON.stringify(projects);
+  const setProjects = (projects) => {
+    localStorage.projects = JSON.stringify(projects);
+  };
+  const setSelectedProject = (projectIndex) => {
+    localStorage.selectedProject = JSON.stringify(projectIndex);
+
+    return projectIndex;
+  };
+  return { getProjects, getSelectedProject, setProjects, setSelectedProject };
 };
-const setSelectedProject = (projectIndex) => {
-  localStorage.selectedProject = JSON.stringify(projectIndex);
-
-  return projectIndex;
-};
-export { getProjects, getSelectedProject, setProjects, setSelectedProject };
+export default storage();

ef5901e2724d082e8c3ff322712091ffab84786f
somoye123 <somoye.ayotunde@gmail.com>
2020-09-22 10:46:25 +0100

    add a buildpage file

---
+++ src/buildPage.js
@@ -0,0 +1,8 @@
+import PageStructure from "./pageStruture";
+import eventListeners from "./eventListeners";
+
+export default () => {
+  PageStructure();
+  eventListeners.chooseProject();
+  eventListeners.addProjectButton();
+};

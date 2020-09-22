0ad02c1f39bbbeed70175ca1fdcee31826bf0b36
somoye123 <somoye.ayotunde@gmail.com>
2020-09-22 10:44:53 +0100

    refactor imported files

--- src/index.js
+++ src/index.js
@@ -1,132 +1,118 @@
-import Task from "./Task";
-import Project from "./Project";
-import PageStructure from "./pageStruture";
-import { setSelectedProject, getSelectedProject } from "./localStorageModule";
+import buildPage from "./buildProject";
 
-const buildPage = () => {
-  PageStructure();
+// const addTaskToProject = (task, projectToUpdate) => {
+//   const foundProject = projects[projectToUpdate];
+//   const project = new Project(foundProject.name);
+//   project.tasks = foundProject._tasks;
+//   project.addTask(task);
+//   projects[projectToUpdate]._tasks = project.tasks;
+//   localStorage.projects = JSON.stringify(projects);
+// };
 
-  let defaultProject = null;
+// const deleteTaskFromProject = (taskIndex, projectIndex) => {
+//   const foundProject = projects[projectIndex];
+//   foundProject._tasks.splice(taskIndex, 1);
+//   projects[projectIndex]._tasks = foundProject._tasks;
+//   localStorage.projects = JSON.stringify(projects);
+// };
 
-  // const addTaskToProject = (task, projectToUpdate) => {
-  //   const foundProject = projects[projectToUpdate];
-  //   const project = new Project(foundProject.name);
-  //   project.tasks = foundProject._tasks;
-  //   project.addTask(task);
-  //   projects[projectToUpdate]._tasks = project.tasks;
-  //   localStorage.projects = JSON.stringify(projects);
-  // };
+// const editTaskButton = document.getElementById("edit-task-0");
+// editTaskButton.addEventListener("click", () => {
+//   formTask.operation = ["edit", 0];
+//   formTask.style.display = "block";
+//   const project = projects[document.getElementById("selected-project").value];
+//   const task = project._tasks[0];
+//   formTask.elements.namedItem("title").value = task._title;
+//   formTask.elements.namedItem("description").value = task._description;
+//   formTask.elements.namedItem("due-date").value = task._dueDate;
+//   formTask.elements.namedItem("priority").value = task._priority;
+//   formTask.elements.namedItem("status").value = task._status;
+//   formTask.elements.namedItem("note").value = task._note;
+//   formTask.elements.namedItem("submit").innerText = "submit task";
+//   // document.getElementById("submit").innerText = "submit task";
+// });
 
-  // const deleteTaskFromProject = (taskIndex, projectIndex) => {
-  //   const foundProject = projects[projectIndex];
-  //   foundProject._tasks.splice(taskIndex, 1);
-  //   projects[projectIndex]._tasks = foundProject._tasks;
-  //   localStorage.projects = JSON.stringify(projects);
-  // };
+// const addTaskButton = document.getElementById("add-task");
 
-  // const editTaskButton = document.getElementById("edit-task-0");
-  // editTaskButton.addEventListener("click", () => {
-  //   formTask.operation = ["edit", 0];
-  //   formTask.style.display = "block";
-  //   const project = projects[document.getElementById("selected-project").value];
-  //   const task = project._tasks[0];
-  //   formTask.elements.namedItem("title").value = task._title;
-  //   formTask.elements.namedItem("description").value = task._description;
-  //   formTask.elements.namedItem("due-date").value = task._dueDate;
-  //   formTask.elements.namedItem("priority").value = task._priority;
-  //   formTask.elements.namedItem("status").value = task._status;
-  //   formTask.elements.namedItem("note").value = task._note;
-  //   formTask.elements.namedItem("submit").innerText = "submit task";
-  //   // document.getElementById("submit").innerText = "submit task";
-  // });
+// const formTask = document.getElementById("new-update-task");
+// formTask.style.display = "none";
 
-  // const addTaskButton = document.getElementById("add-task");
+// addTaskButton.addEventListener("click", () => {
+//   formTask.operation = ["add"];
+//   formTask.style.display = "block";
+//   formTask.reset();
+// });
 
-  // const formTask = document.getElementById("new-update-task");
-  // formTask.style.display = "none";
+// formTask.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const title = formTask.elements.namedItem("title").value;
+//   const description = formTask.elements.namedItem("description").value;
+//   const dueDate = formTask.elements.namedItem("due-date").value;
+//   const priority = formTask.elements.namedItem("priority").value;
+//   const status = formTask.elements.namedItem("status").value;
+//   const note = formTask.elements.namedItem("note").value;
+//   const project = document.getElementById("selected-project").value;
+//   if (title && description && dueDate && priority && status && note) {
+//     const task = new Task(title, description, dueDate, priority, status, note);
 
-  // addTaskButton.addEventListener("click", () => {
-  //   formTask.operation = ["add"];
-  //   formTask.style.display = "block";
-  //   formTask.reset();
-  // });
+//     if (formTask.operation[0] === "add") {
+//       addTaskToProject(task, project);
+//     } else {
+//       projects[0]._tasks[formTask.operation[1]] = task;
+//     }
 
-  // formTask.addEventListener("submit", (event) => {
-  //   event.preventDefault();
-  //   const title = formTask.elements.namedItem("title").value;
-  //   const description = formTask.elements.namedItem("description").value;
-  //   const dueDate = formTask.elements.namedItem("due-date").value;
-  //   const priority = formTask.elements.namedItem("priority").value;
-  //   const status = formTask.elements.namedItem("status").value;
-  //   const note = formTask.elements.namedItem("note").value;
-  //   const project = document.getElementById("selected-project").value;
-  //   if (title && description && dueDate && priority && status && note) {
-  //     const task = new Task(title, description, dueDate, priority, status, note);
+//     localStorage.projects = JSON.stringify(projects);
+//     formTask.reset();
+//     formTask.style.display = "none";
+//   } else {
+//     alert("Fill all informations correctly ");
+//   }
+// });
 
-  //     if (formTask.operation[0] === "add") {
-  //       addTaskToProject(task, project);
-  //     } else {
-  //       projects[0]._tasks[formTask.operation[1]] = task;
-  //     }
+// const deleteTaskButton = document.getElementById("delete-task-1");
 
-  //     localStorage.projects = JSON.stringify(projects);
-  //     formTask.reset();
-  //     formTask.style.display = "none";
-  //   } else {
-  //     alert("Fill all informations correctly ");
-  //   }
-  // });
+// deleteTaskButton.addEventListener("click", () => {
+//   deleteTaskFromProject(0, 0);
+// });
 
-  // const deleteTaskButton = document.getElementById("delete-task-1");
+// const addProjectButton = document.getElementById("add-project");
 
-  // deleteTaskButton.addEventListener("click", () => {
-  //   deleteTaskFromProject(0, 0);
-  // });
+// const formProject = document.getElementById("new-project");
+// formProject.style.display = "none";
 
-  // const addProjectButton = document.getElementById("add-project");
+// addProjectButton.addEventListener("click", () => {
+//   formProject.style.display = "block";
+// });
 
-  // const formProject = document.getElementById("new-project");
-  // formProject.style.display = "none";
+// formProject.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const title = formProject.elements.namedItem("title").value;
+//   const project = new Project(title);
+//   if (title) {
+//     projects.push(project);
+//     localStorage.projects = JSON.stringify(projects);
+//     formProject.reset();
+//     formProject.style.display = "none";
+//   } else {
+//     alert("Fill all informations correctly ");
+//   }
+// });
 
-  // addProjectButton.addEventListener("click", () => {
-  //   formProject.style.display = "block";
-  // });
+// const deleteProjectButton = document.getElementById("delete-project");
 
-  // formProject.addEventListener("submit", (event) => {
-  //   event.preventDefault();
-  //   const title = formProject.elements.namedItem("title").value;
-  //   const project = new Project(title);
-  //   if (title) {
-  //     projects.push(project);
-  //     localStorage.projects = JSON.stringify(projects);
-  //     formProject.reset();
-  //     formProject.style.display = "none";
-  //   } else {
-  //     alert("Fill all informations correctly ");
-  //   }
-  // });
+// deleteProjectButton.addEventListener("click", () => {
+//   const projectIndex = document.getElementById("selected-project").value;
+//   projects.splice(projectIndex, 1);
+//   localStorage.projects = JSON.stringify(projects);
+// });
 
-  // const deleteProjectButton = document.getElementById("delete-project");
+// const formProject = document.getElementById("new-project");
+// formProject.style.display = "none";
 
-  // deleteProjectButton.addEventListener("click", () => {
-  //   const projectIndex = document.getElementById("selected-project").value;
-  //   projects.splice(projectIndex, 1);
-  //   localStorage.projects = JSON.stringify(projects);
-  // });
+// const addProjectButton = document.getElementById("add-project");
 
-  const chooseProject = document.getElementById("selected-project");
-  chooseProject.addEventListener(
-    "change",
-    function () {
-      setSelectedProject(this.value);
-      const content = document.getElementById("content");
-      while (content.firstChild) {
-        content.removeChild(content.lastChild);
-      }
-      buildPage();
-    },
-    false
-  );
-};
+// addProjectButton.addEventListener("click", () => {
+//   formProject.style.display = "block";
+// });
 
 buildPage();

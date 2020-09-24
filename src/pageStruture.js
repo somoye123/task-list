01531c79f7e27fee2db2f377a2200ec278d56025
Hasan Ozovali <ozovalihasan@gmail.com>
2020-09-24 11:54:21 +0300

    Update edit task button to use updated task form

--- src/pageStruture.js
+++ src/pageStruture.js
@@ -22,6 +22,7 @@ const PageStructure = () => {
   projectsSelect.innerHTML = projectsSelectOptions;
   projectsSelect.value =
     storage.getSelectedProject() || storage.setSelectedProject(0);
+
   const addNewProject = document.createElement("button");
   addNewProject.innerText = "Add new project";
   addNewProject.classList.add("btn", "btn-success");
@@ -44,6 +45,7 @@ const PageStructure = () => {
   if (projects().length > 0) {
     const taskUnorderList = document.createElement("ul");
     taskUnorderList.id = "task-order-list";
+
     const selectedProject = projects()[projectsSelect.value];
 
     let selectedProjectLists = `
@@ -58,7 +60,6 @@ const PageStructure = () => {
     selectedProject &&
       selectedProject._tasks.forEach((task, taskIndex) => {
         selectedProjectLists += `
-
         <h5>${task._title}</h5>
         <h5>${task._description}</h5>
         <h5>${task._dueDate}</h5>
@@ -66,12 +67,11 @@ const PageStructure = () => {
         <h5>${task._status}</h5>
         <h5>${task._note}</h5>
         <div class="d-flex ">
-          <button class="edit-task mx-2 btn btn-warning" id="edit-task-${taskIndex}" value='${taskIndex}'>
+          <button class="edit-task mx-2 btn btn-warning" id="edit-task-${taskIndex}" value='${taskIndex}' data-toggle="modal" data-target="#taskModal">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
             </svg>
           </button>
-
           <button class="delete-task mx-2 btn btn-danger" id="delete-task-${taskIndex}" value='${taskIndex}'>
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
@@ -154,86 +154,84 @@ const PageStructure = () => {
   });
 
   let taskFormModalContent = `
-
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <form id="new-update-task">
-        <div class="modal-body">
-          <div class="form-group">
-            <label for="new-project-modal">Name</label>
-            <input
-              type="text"
-              class="form-control"
-              id="title"
-              name="title"
-            />
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <form id="new-update-task">
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="new-project-modal">Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="title"
+                name="title"
+              />
+            </div>
+            <div class="form-group">
+              <label for="new-project-modal">Description</label>
+              <textarea 
+                class="form-control"
+                id="description"
+                name="description"
+              ></textarea>
+            </div>
+            <div class="form-group">
+              <label for="new-project-modal">Due-Date</label>
+              <input
+                type="date"
+                class="form-control"
+                id="due-date"
+                name="due-date"
+              />
+            </div>
+            <div class="form-group">
+              <label for="new-project-modal">Priority</label>
+              <select
+                type="text"
+                class="form-control"
+                id="priority-status"
+                name="priority"
+                required
+              >
+              ${prioritySelectOptions}
+              </select>
+            </div>        
+            <div class="form-group">
+              <label for="new-project-modal">Status</label>
+              <select
+                type="text"
+                class="form-control"
+                id="status"
+                name="status"
+                required
+              >
+              ${statusSelectOptions}
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="new-project-modal">Note</label>
+              <textarea
+                type="text"
+                class="form-control"
+                id="note"
+                name="note"
+              ></textarea>
+            </div>                
           </div>
-          <div class="form-group">
-            <label for="new-project-modal">Description</label>
-            <textarea 
-              class="form-control"
-              id="description"
-              name="description"
-            ></textarea>
-          </div>
-          <div class="form-group">
-            <label for="new-project-modal">Due-Date</label>
-            <input
-              type="date"
-              class="form-control"
-              id="due-date"
-              name="due-date"
-            />
-          </div>
-          <div class="form-group">
-            <label for="new-project-modal">Priority</label>
-            <select
-              type="text"
-              class="form-control"
-              id="priority-status"
-              name="priority"
-              required
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
             >
-            ${prioritySelectOptions}
-            </select>
-          </div>        
-          <div class="form-group">
-            <label for="new-project-modal">Status</label>
-            <select
-              type="text"
-              class="form-control"
-              id="status"
-              name="status"
-              required
-            >
-            ${statusSelectOptions}
-            </select>
+              Close
+            </button>
+            <button type="submit" data-toggle="modal" data-target="#taskModal" class="btn btn-success" name="task-submit" ></button>
           </div>
-          <div class="form-group">
-            <label for="new-project-modal">Note</label>
-            <textarea
-              type="text"
-              class="form-control"
-              id="note"
-              name="note"
-            ></textarea>
-          </div>                
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-dismiss="modal"
-          >
-            Close
-          </button>
-          <button type="submit" data-toggle="modal" data-target="#taskModal" class="btn btn-success" name="task-submit" ></button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
   `;
-
   taskFormModal.innerHTML = taskFormModalContent;
 
   content.appendChild(projectContainer);

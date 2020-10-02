3d90cfbed83144cb3ba77acadafec147f73db02b
Hasan Ozovali <ozovalihasan@gmail.com>
2020-10-02 13:51:33 +0300

    Fix errors found by linters

--- __test__/Task.test.js
+++ __test__/Task.test.js
@@ -1,57 +1,57 @@
-import Task from "../src/Task";
+import Task from '../src/Task';
 
-describe("Task", () => {
+describe('Task', () => {
   let task;
   beforeEach(() => {
     task = new Task(
-      "title",
-      "description",
-      "dueDate",
-      "priority",
-      "status",
-      "note"
+      'title',
+      'description',
+      'dueDate',
+      'priority',
+      'status',
+      'note',
     );
   });
 
-  test("create task", () => {
+  test('create task', () => {
     expect(task instanceof Task).toBe(true);
     expect(task).toEqual({
-      _title: "title",
-      _description: "description",
-      _dueDate: "dueDate",
-      _priority: "priority",
-      _status: "status",
-      _note: "note",
+      _title: 'title',
+      _description: 'description',
+      _dueDate: 'dueDate',
+      _priority: 'priority',
+      _status: 'status',
+      _note: 'note',
     });
   });
 
-  test("setter of title", () => {
-    task.title = "new title";
-    expect(task.title).toBe("new title");
+  test('setter of title', () => {
+    task.title = 'new title';
+    expect(task.title).toBe('new title');
   });
 
-  test("setter of description", () => {
-    task.description = "new description";
-    expect(task.description).toBe("new description");
+  test('setter of description', () => {
+    task.description = 'new description';
+    expect(task.description).toBe('new description');
   });
 
-  test("setter of dueDate", () => {
-    task.dueDate = "new dueDate";
-    expect(task.dueDate).toBe("new dueDate");
+  test('setter of dueDate', () => {
+    task.dueDate = 'new dueDate';
+    expect(task.dueDate).toBe('new dueDate');
   });
 
-  test("setter of priority", () => {
-    task.priority = "new priority";
-    expect(task.priority).toBe("new priority");
+  test('setter of priority', () => {
+    task.priority = 'new priority';
+    expect(task.priority).toBe('new priority');
   });
 
-  test("setter of status", () => {
-    task.status = "new status";
-    expect(task.status).toBe("new status");
+  test('setter of status', () => {
+    task.status = 'new status';
+    expect(task.status).toBe('new status');
   });
 
-  test("setter of note", () => {
-    task.note = "new note";
-    expect(task.note).toBe("new note");
+  test('setter of note', () => {
+    task.note = 'new note';
+    expect(task.note).toBe('new note');
   });
 });
